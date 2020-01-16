e233f19f05a885289e08d5db61855ad4833e4fca
L-kcirtaP <116010151@link.cuhk.edu.cn>
2020-01-16 16:27:07 +0800

    qsort

--- leetcode/21_merge-two-sorted-lists.cpp
+++ leetcode/21_merge-two-sorted-lists.cpp
@@ -1,17 +1,18 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
         if (!l1 or !l2) {
             if (!l1) {return l2;}
             else {return l1;}
+            
         } else {
             ListNode* l = new ListNode(0);
             ListNode* head = l;
---
+++ quick-sort.py
@@ -0,0 +1,28 @@
+def partition(array, start, end):
+    pivot = array[start]
+    p_1 = start + 1
+    p_2 = end
+
+    while p_1 <= p_2:
+        if array[p_1] <= pivot:
+            p_1 += 1
+        if array[p_2] > pivot:
+            p_2 -= 1
+    
+        if p_1 < p_2:
+            array[p_1], array[p_2] = array[p_2], array[p_1]
+    
+    array[start], array[p_2] = array[p_2], array[start]
+
+    return p_2
+
+
+def q_sort(array, start, end):
+    if start < end:
+        pivot = partition(array, start, end)
+
+        q_sort(array, start, pivot-1)
+        q_sort(array, pivot+1, end)
+
+array = [4, 0, 2, 5, 7, 3, 10]
+q_sort(array, 0, len(array)-1)
