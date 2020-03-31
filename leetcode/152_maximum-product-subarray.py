bee101a062bc2f60b9885f1a086ecb6c6df10ad1
L-kcirtaP <116010151@link.cuhk.edu.cn>
2020-03-31 21:34:14 +0800

    problem 152

--- leetcode/152_maximum-product-subarray.py
+++ leetcode/152_maximum-product-subarray.py
@@ -1,6 +1,20 @@
+def maxProductCore(nums) -> int:
+	cur_max = max_so_far = nums[0]
+	max_abs = 1 if nums[0] == 0 else nums[0]
+	for i in nums[1:]:
+		if i == 0:
+			cur_max = i
+			max_abs = 1
+		else:
+			max_abs = max_abs * i
+			if max_abs > 0:
+				cur_max = max_abs
+			else:
+				cur_max = max(cur_max, i)
+			max_so_far = max(cur_max, max_so_far)
+	return max_so_far
+
 def maxProduct(nums) -> int:
-	
-	
-	return 0
+	return max(maxProductCore(nums), maxProductCore(nums[::-1]))
 
-
+print(maxProduct([2,-5,-2,-4,3]))
