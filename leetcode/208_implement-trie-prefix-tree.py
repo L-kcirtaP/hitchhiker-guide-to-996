3e356e16d2c489501aeb1e57f6fa6afa2fc2c852
L-kcirtaP <116010151@link.cuhk.edu.cn>
2020-04-28 22:59:37 +0800

    problem 208

--- leetcode/208_implement-trie-prefix-tree.py
+++ leetcode/208_implement-trie-prefix-tree.py
@@ -1,47 +1,90 @@
 class Trie:
+
     class TrieNode:
         def __init__(self, val, isWord=False):
             self.val = val
             self.isWord = isWord
             self.children = []
-    root = None
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
-        return
+        self.root = self.TrieNode('')
         
+
+    def _constructWordTree(self, word: str) -> TrieNode:
+        if not word:
+            return None
+
+        root = self.TrieNode(word[0])
+        next_node = self._constructWordTree(word[1:])
+        if not next_node:
+            root.isWord = True
+        else:
+            root.children.append(next_node)
+        return root
+
 
     def insert(self, word: str) -> None:
         """
         Inserts a word into the trie.
         """
+        if not word:
+            return
 
-        return
-        
+        root = self.root
+        for i in range(len(word)):
+            flg = False
+            for child in root.children:
+                if word[i] == child.val:
+                    root = child
+                    flg = True
+                    if i == len(word) - 1:
+                        root.isWord = True
+            if not flg:
+                break
 
+        root.children.append(self._constructWordTree(word[i:]))
+
+
+    def _searchTree(self, root: TrieNode, word: str) -> bool:
+        if not word:
+            return root.isWord
+
+        for child in root.children:
+            if word[0] == child.val:
+                root = child
+                return self._searchTree(root, word[1:])
+
+        return False
+
+   
     def search(self, word: str) -> bool:
-        """
-        Returns if the word is in the trie.
-        """
-
-        return
+        return self._searchTree(self.root, word)
         
+
+    def _startsWith(self, root: TrieNode, word: str) -> bool:
+        if not word:
+            return True
+
+        for child in root.children:
+            if word[0] == child.val:
+                root = child
+                return self._startsWith(root, word[1:])
+
+        return False
+
 
     def startsWith(self, prefix: str) -> bool:
-        """
-        Returns if there is any word in the trie that starts with the given prefix.
-        """
-
-        return
+        return self._startsWith(self.root, prefix)
         
 
 
 word = 'hello'
-prefix = 'hell'
+prefix = 'hel'
 # Your Trie object will be instantiated and called as such:
 obj = Trie()
 obj.insert(word)
 param_2 = obj.search(word)
+print(param_2)
 param_3 = obj.startsWith(prefix)
+print(param_3)
+obj.insert('haha')
